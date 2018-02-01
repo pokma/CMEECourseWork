03509d284a4f6ad9b60826922b461bdbf2ae25ec
Peter Sander <sander@unice.fr>
2018-02-01 11:17:35 +0100

    Added TreeHeight.R as well as TreeHeights.R to handle input from a data file of tree heights.

---
+++ Week1/Code/TreeHeights.R
@@ -0,0 +1,23 @@
+# Calculates heights of trees given distance of each tree
+# from its base and angle to its top, using the trigonometric formula
+#
+# height = distance * tan(radians)
+#
+# Tree species, distance, and angle are read from a hard-wired data file.
+# Needs to source prerequisite TreeHeight function.
+#
+# OUTPUT:
+# A data frame with the following columns:
+# Species, Distance.m, Angle.degrees, Tree.Height.m
+# <pedantic>
+# The last column name has redundant info and should be just Height.m
+# </pedantic>
+
+TreeHeights <- function() {
+  treeFrame <- read.csv("../Data/trees.csv", header=TRUE)
+  Height.m <- TreeHeight(treeFrame$Distance.m, treeFrame$Angle.degrees)
+  treeFrame$Tree.Height.m <- Height.m
+  # don't want row numbers in the output
+  write.csv(treeFrame, '../Results/TreeHts.csv', row.names=FALSE)
+  return(treeFrame)
+}
