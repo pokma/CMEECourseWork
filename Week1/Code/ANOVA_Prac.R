5585eeb33b5028ae02d7cae5ef2a5340dbd950e0
kas4417 <kim.sander17@imperial.ac.uk>
2018-02-06 15:52:41 +0000

    Yesterday's and today's work. More stats. Haven't quite finished the ANOVA_Prac and Regression scripts yet, but will go over those some more tomorrow.

---
+++ Week1/Code/ANOVA_Prac.R
@@ -0,0 +1,73 @@
+### ANOVA (Analysis of variance) ###
+
+# Load data into data frame and examine data
+mammals <- read.csv("../Data/MammalData.csv")
+str(mammals)
+summary(mammals)
+
+######## PLOT DATA ########
+# Before fitting any models, plot the data to see if the means withing the groupings of interest look different, check if varience looks similar (constant normal variance). Use box and whisker plots.
+plot(meanCvalue ~ TrophicLevel, data = mammals) #differences in genome size between trophic levels
+mammals$logCvalue <- log(mammals$meanCvalue)
+boxplot(mammals$logCvalue ~ TrophicLevel, data = mammals)
+
+plot(meanCvalue ~ GroundDwelling, data = mammals) #differences between grounddwelling and other species
+boxplot(mammals$logCvalue ~ GroundDwelling, data = mammals)
+
+#### Differences in means with barplots ####
+# Get standard error of the mean from a set of values (x)
+seMean <- function(logCvalue){
+logCvalue <- na.omit(mammals$logCvalue) # get rid of missing values
+se <- sqrt(var(logCvalue)/length(logCvalue)) # calculate the standard error
+return(se) # tell the function to return the standard error
+}
+
+##### Plot for TrophicLevel #####
+# Now use function tapply: it splits a variable up into groups from a factor and calculates stats on each group using a function
+trophMeans <- tapply(mammals$logCvalue, mammals$TrophicLevel, FUN = mean, na.rm=TRUE)
+print(trophMeans)
+trophSE <- tapply(mammals$logCvalue, mammals$TrophicLevel, FUN=seMean)
+print(trophSE)
+
+## Put values together on the plot ##
+# Get the upper and lower limits of the error bars
+upperSE <- trophMeans + trophSE
+lowerSE <- trophMeans - trophSE
+# Get a barplot: this function can report where the middle of the bars are on the x axis. Set the y axis limits to contain the error bars
+barMids <- barplot(trophMeans, ylim=c(0, max(upperSE)),ylab="log C value (pg)")
+# Add error bars using function: draws arrows between the points (x0,y0) and (x1,y1). Arrow heads at each end (code=3) and at 90 degree angles
+arrows(barMids, upperSE, barMids, lowerSE, ang=90, code=3)
+
+
+
+##### Plot for GroundDwelling (same process, different values) #####
+groundMeans <- tapply(mammals$logCvalue, mammals$GroundDwelling, FUN = mean, na.rm=TRUE)
+print(trophMeans)
+groundSE <- tapply(mammals$logCvalue, mammals$GroundDwelling, FUN=seMean)
+print(groundSE)
+
+upperSE <- groundMeans + groundSE
+lowerSE <- groundMeans - groundSE
+barMids <- barplot(groundMeans, ylim=c(0, max(upperSE)),ylab="log C value (pg)")
+arrows(barMids, upperSE, barMids, lowerSE, ang=90, code=3)
+
+
+
+####### Alternative to barplots #######
+# Use the gplots package to create plots of group means and confidence intervals.
+# Load the gplots package
+install.packages("gplots") #(install package)
+library(gplots)
+
+# Get plots of group means and standard errors
+par(mfrow=c(1,2))
+plotmeans(logCvalue ~ TrophicLevel, data = mammals, p=0.95, connect=FALSE)
+plotmeans(logCvalue ~ GroundDwelling, data = mammals, p=0.95, connect=FALSE)
+
+
+
+######### ANALYSIS OF VARIANCE ##########
+# Use linear model to test whether differences are significant
+trophicLM <- lm(logCvalue ~ TrophicLevel, data = mammals)
+summary(trophicLM) #coefficients
+anova(trophicLM) #terms
---
+++ Week1/Code/Regression.R
@@ -0,0 +1,62 @@
+### Linear Models: Regression
+
+# Exploring the data: use indices to access data frame content
+dat <- data.frame(A = c ("a", "b", "c", "d", "e"), B= c(1,2,3,4,5))
+# select row 1 (all columns selected)
+dat[1,]
+# select column 2 (all rows selected)
+dat[,2]
+# row 2, column 1
+dat[2,1]
+
+# Load data
+genome <- read.csv("../Data/GenomeSize.csv")
+
+# Record columns of key variables of interest
+morpho <- c(4,7,8,12,14)
+pairs(genome[, morpho], col=genome$Suborder)
+
+### CORRELATION ###
+cor(genome[, morpho], use = "pairwise") # the use="pairwise" tells R to omit observations with missing data
+cor.test(genome$GenomeSize, genome$TotalLength, use = "pairwise")
+
+# Create logged version of the variables of interest
+genome$logGS <- log(genome$GenomeSize)
+genome$logBW <- log(genome$BodyWeight)
+genome$logTL <- log(genome$TotalLength)
+genome$logFL <- log(genome$ForewingLength)
+genome$logFA <- log(genome$ForewingArea)
+# Create new variable containing columns of these logged variales
+str(genome)
+logmorpho <- c(22,23,24,25,26)
+# pairs and cor test
+pairs(genome[, logmorpho], col=genome$Suborder)
+cor(genome[, logmorpho], use = "pairwise")
+cor.test(genome$logGS, genome$logTL, use = "pairwise")
+
+### REGRESSION ###
+nullModelDragon <- lm(logBW ~ 1, data = genome, subset = Suborder == "Anisoptera")
+genomeSizeModelDragon <- lm(logBW ~ logGS, data = genome, subset = Suborder == "Anisoptera")
+# Look at coefficients of model
+summary(genomeSizeModelDragon)
+# Look at terms of model
+anova(genomeSizeModelDragon)
+# Residuals: sums of their squares from the two models
+sum(resid(nullModelDragon) ^ 2)
+sum(resid(genomeSizeModelDragon) ^ 2)
+
+
+## For the damselflies instead of dragonflies now
+nullModelDamsel <- lm(logBW ~ 1, data = genome, subset = Suborder == "Zygoptera")
+genomeSizeModelDamsel <- lm(logBW ~ logGS, data = genome, subset = Suborder == "Zygoptera")
+# Look at coefficients of model
+summary(genomeSizeModelDamsel)
+# Look at terms of model
+anova(genomeSizeModelDamsel)
+# Residuals: sums of their squares from the two models
+sum(resid(nullModelDamsel) ^ 2)
+sum(resid(genomeSizeModelDamsel) ^ 2)
+
+
+### PLOTTING THE MODEL ###
+plot(log(genome$logBW),log(genome$logGS),pch=20, ylab="log Body Weight", xlab="log Genome Size")
