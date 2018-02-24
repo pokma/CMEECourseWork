f77b77db45724b88301fa03a0af5371d49480f82
Peter Sander <sander@unice.fr>
2018-02-24 14:50:38 +0100

    Fixed up the seMean function in ANOVA_Prac.R.

--- Week1/Code/ANOVA_Prac.R
+++ Week1/Code/ANOVA_Prac.R
@@ -12,19 +12,20 @@ mammals$logCvalue <- log(mammals$meanCvalue)
 boxplot(mammals$logCvalue ~ TrophicLevel, data = mammals)
 
 plot(meanCvalue ~ GroundDwelling, data = mammals) #differences between grounddwelling and other species
+boxplot(meanCvalue ~ GroundDwelling, data = mammals)
 boxplot(mammals$logCvalue ~ GroundDwelling, data = mammals)
 
 #### Differences in means with barplots ####
 # Get standard error of the mean from a set of values (x)
-seMean <- function(logCvalue){
-logCvalue <- na.omit(mammals$logCvalue) # get rid of missing values
-se <- sqrt(var(logCvalue)/length(logCvalue)) # calculate the standard error
-return(se) # tell the function to return the standard error
+seMean <- function(x) {  # corrected
+    x <- na.omit(x) # get rid of missing values
+    se <- sqrt(var(x)/length(x)) # calculate the standard error
+    return(se) # tell the function to return the standard error
 }
 
 ##### Plot for TrophicLevel #####
 # Now use function tapply: it splits a variable up into groups from a factor and calculates stats on each group using a function
-trophMeans <- tapply(mammals$logCvalue, mammals$TrophicLevel, FUN = mean, na.rm=TRUE)
+trophMeans <- tapply(mammals$logCvalue, mammals$TrophicLevel, FUN=mean, na.rm=TRUE)
 print(trophMeans)
 trophSE <- tapply(mammals$logCvalue, mammals$TrophicLevel, FUN=seMean)
 print(trophSE)
@@ -33,9 +34,11 @@ print(trophSE)
 # Get the upper and lower limits of the error bars
 upperSE <- trophMeans + trophSE
 lowerSE <- trophMeans - trophSE
-# Get a barplot: this function can report where the middle of the bars are on the x axis. Set the y axis limits to contain the error bars
-barMids <- barplot(trophMeans, ylim=c(0, max(upperSE)),ylab="log C value (pg)")
-# Add error bars using function: draws arrows between the points (x0,y0) and (x1,y1). Arrow heads at each end (code=3) and at 90 degree angles
+# Get a barplot: this function can report where the middle of the bars are on the x axis.
+# Set the y axis limits to contain the error bars
+barMids <- barplot(trophMeans, ylim=c(0, max(upperSE)), ylab="log C value (pg)")
+# Add error bars using function: draws arrows between the points (x0,y0) and (x1,y1).
+# Arrow heads at each end (code=3) and at 90 degree angles
 arrows(barMids, upperSE, barMids, lowerSE, ang=90, code=3)
 
 
@@ -67,7 +70,12 @@ plotmeans(logCvalue ~ GroundDwelling, data = mammals, p=0.95, connect=FALSE)
 
 
 ######### ANALYSIS OF VARIANCE ##########
-# Use linear model to test whether differences are significant
+# Use linear model to test whether trophic differences are significant
 trophicLM <- lm(logCvalue ~ TrophicLevel, data = mammals)
 summary(trophicLM) #coefficients
 anova(trophicLM) #terms
+
+# Use linear model to test whether ground-dwelling differences are significant
+groundDwellingLM <- lm(logCvalue ~ GroundDwelling, data = mammals)
+summary(groundDwellingLM) #coefficients
+anova(groundDwellingLM) #terms
